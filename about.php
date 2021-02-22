36a48320f5a863a955ec54d23d0f6586243397be
Samson <naanman10@gmail.com>
2021-02-22 14:38:56 +0100

    second commit

---
+++ about.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <script src="./Bootstrap-4/J_query/jquery-3.5.1.js"></script>
+    <link rel="stylesheet" href="./Bootstrap-4/CSS/bootstrap.min.css">
+    <script src="./Bootstrap-4/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h2 align="center">About us</h2>
+        <p align="center" class="lead">We are African</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 bg-danger">
+                    <h1>Our Mission</h1>
+                </div>
+                <div class="col-md-4 bg-success">
+                    <h1>Our Vision </h1>
+                </div>
+                <div class="col-md-4 bg-primary">
+                    <h1>Our Values</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
